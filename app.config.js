a7dec8ed3e7db41fba0faeba52c405368b02e3a8
alexanderoskin <alexander.oskin@gmail.com>
2026-04-16 10:39:23 +0300

    Disable navigation bar contrast enforcement on Android

--- app.config.js
+++ app.config.js
@@ -66,7 +66,7 @@ module.exports = {
             {
                 android: {
                     parentTheme: 'Default',
-                    enforceNavigationBarContrast: true,
+                    enforceNavigationBarContrast: false,
                 },
             },
         ],
